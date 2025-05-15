ee1148f565c9e8d81fac62395537bd715fd12843
Unathi Mathokazi <unathimathokaziu@gmail.com>
2025-05-15 20:39:12 +0200

    Set up Tailwind CSS with input and output files

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./src/**/*.{html,js,jsx}"],  // Adjust according to your folder structure
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  }
+  
