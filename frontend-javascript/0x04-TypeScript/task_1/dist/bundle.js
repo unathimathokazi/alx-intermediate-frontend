279f53afae86cdf068a2b69cf06d5e31dfba9360
Unathi Mathokazi <unathimathokaziu@gmail.com>
2025-05-22 20:12:02 +0200

    Add task_1 with Teacher interface

---
+++ frontend-javascript/0x04-TypeScript/task_1/dist/bundle.js
@@ -0,0 +1,18 @@
+/******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+/*!********************!*\
+  !*** ./js/main.ts ***!
+  \********************/
+
+var teacher3 = {
+    firstName: 'John',
+    lastName: 'Doe',
+    fullTimeEmployee: false,
+    location: 'London',
+    contract: false,
+};
+console.log(teacher3);
+
+/******/ })()
+;
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiYnVuZGxlLmpzIiwibWFwcGluZ3MiOiI7Ozs7O0FBQWE7QUFDYjtBQUNBO0FBQ0E7QUFDQTtBQUNBO0FBQ0E7QUFDQTtBQUNBIiwic291cmNlcyI6WyJ3ZWJwYWNrOi8vc3R1ZGVudC1pbnRlcmZhY2UtcHJvamVjdC8uL2pzL21haW4udHMiXSwic291cmNlc0NvbnRlbnQiOlsiXCJ1c2Ugc3RyaWN0XCI7XG52YXIgdGVhY2hlcjMgPSB7XG4gICAgZmlyc3ROYW1lOiAnSm9obicsXG4gICAgbGFzdE5hbWU6ICdEb2UnLFxuICAgIGZ1bGxUaW1lRW1wbG95ZWU6IGZhbHNlLFxuICAgIGxvY2F0aW9uOiAnTG9uZG9uJyxcbiAgICBjb250cmFjdDogZmFsc2UsXG59O1xuY29uc29sZS5sb2codGVhY2hlcjMpO1xuIl0sIm5hbWVzIjpbXSwic291cmNlUm9vdCI6IiJ9
